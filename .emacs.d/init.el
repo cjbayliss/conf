76a1d060b0505b9a9f2c59e990a1b16da4466407
Christopher Bayliss <cjb@cjb.sh>
2019-04-30 11:16:49 +1000

    remove custom faces, set bg to dark

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,11 +16,8 @@
       c-default-style "linux")
 
 ;; set the default fill-column char width
-(setq-default fill-column 79)
-
-;; enable line numbers & highlight line
-(global-display-line-numbers-mode)
-(global-hl-line-mode)
+(setq-default fill-column 79
+              frame-background-mode 'dark)
 
 ;; ERC config
 (with-eval-after-load "erc"
@@ -59,7 +56,6 @@
 
   ;; fix ERC prompt colors
   (custom-set-faces '(erc-prompt-face ((t (:foreground "brightwhite" :background nil :weight bold))))
-                    '(erc-timestamp-face ((t (:foreground "brightwhite" :weight bold))))
                     '(erc-input-face ((t (:foreground "white"))))
                     '(erc-my-nick-face ((t (:foreground "brightred" :weight bold)))))
 
@@ -94,14 +90,14 @@
   ;; people i associate their with a color from previous nick color scripts
   (setq my-custom-colors '(("Unit193" . "color-92")
                            ("twb" . "color-226")
-                           ("codingquark" . "color-194")
+                           ("codingquark" . "color-157")
                            ("parsnip" . "color-69")
                            ("parsnip0" . "color-69")
                            ("bremner" . "color-226")
                            ("bpalmer". "color-241")
                            ("jlf" . "color-191")
-                           ("fsbot" . "color-225")
-                           ("cjb" . "color-85")))
+                           ("fsbot" . "color-219")
+                           ("cjb" . "brightred")))
 
   (defun my/return-color (string)
     "return color for STRING"
@@ -129,32 +125,5 @@
 
 ;; beleive it or not, this **doesn't** increase emacs init time
 (custom-set-faces
- '(mode-line ((t (:foreground "white" :slant oblique))))
- '(mode-line-buffer-id ((t (:foreground "red" :background nil :weight bold))))
- '(mode-line-inactive ((t (:foreground "white"))))
- '(custom-state ((t (:foreground "brightgreen"))))
- '(custom-variable-tag ((t (:foreground "blue" :weight bold))))
- '(font-lock-builtin-face ((t (:foreground "brightyellow"))))
- '(font-lock-comment-face ((t (:foreground "brightblack"))))
- '(font-lock-constant-face ((t (:foreground "brightmagenta"))))
- '(font-lock-function-name-face ((t (:foreground "brightblue"))))
- '(font-lock-keyword-face ((t (:foreground "brightred"))))
- '(font-lock-preprocessor-face ((t (:foreground "brightcyan"))))
- '(font-lock-string-face ((t (:foreground "brightgreen"))))
- '(font-lock-type-face ((t (:foreground "brightyellow"))))
- '(font-lock-variable-name-face ((t (:foreground "brightred"))))
- '(header-line ((t (:foreground "brightwhite" :slant oblique :weight bold))))
- '(hl-line ((t (:background "color-237"))))
- '(isearch ((t (:background "brightyellow" :foreground "black"))))
- '(lazy-highlight ((t (:background "red" :foreground "black"))))
- '(line-number-current-line ((t (:background "color-237" :foreground "yellow"))))
- '(link ((t (:foreground "color-117" :underline t))))
- '(minibuffer-prompt ((t (:foreground "brightwhite" :weight bold))))
- '(region ((t (:inverse-video t))))
- '(rst-level-1 ((t (:weight bold))))
- '(rst-level-2 ((t (:weight bold))))
- '(rst-level-3 ((t (:weight bold))))
- '(rst-level-4 ((t (:weight bold))))
- '(rst-level-5 ((t (:weight bold))))
- '(trailing-whitespace ((t (:background "red"))))
- '(widget-field ((t (:background "white" :foreground "black")))))
+ '(mode-line-buffer-id ((t (:foreground "red" :background nil :weight bold :slant oblique))))
+ '(region ((t (:inverse-video t)))))
