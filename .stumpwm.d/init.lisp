43d92a222b8701b350af71484d16b4f45e7bf1f4
Christopher Bayliss <cjb@cjb.sh>
2019-04-30 19:51:23 +1000

    expand stumpwm init

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,2 +1,17 @@
+;; load quicklisp for cool kids stuff
+(load "~/quicklisp/setup.lisp")
+
 (set-prefix-key (kbd "C-z"))
-(setf *window-border-style* :none)
+(setf *startup-message* nil
+      *window-border-style* :none
+      *message-window-gravity* :bottom-right
+      *input-window-gravity* :bottom-right)
+
+(ql:quickload "clx-truetype")
+(load-module "ttf-fonts")
+;; uncomment this for first run, or if you install a new font
+;;(xft:cache-fonts)
+(set-font (make-instance 'xft:font
+                         :family "Iosevka Term Slab"
+                         :subfamily "Regular"
+                         :size 11))
