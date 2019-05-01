890875992940cf39cf12e8114f8a1f0d49adcef3
Christopher Bayliss <cjb@cjb.sh>
2019-05-01 17:53:34 +1000

    add keybindings

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -2,10 +2,59 @@
 (load "~/quicklisp/setup.lisp")
 
 (set-prefix-key (kbd "C-z"))
-(setf *startup-message* nil
-      *window-border-style* :none
-      *message-window-gravity* :bottom-right
-      *input-window-gravity* :bottom-right)
+(setf *window-border-style* :none)
+
+;; start dmenu with C-z C-d
+(define-key *root-map* (kbd "C-d") "exec dmenu_run -b -fn 'Iosevka Term Slab'")
+;; start terminal with C-z c or C-z C-c
+(define-key *root-map* (kbd "c") "exec sst")
+(define-key *root-map* (kbd "C-c") "exec sst")
+
+;; brightness control
+(defcommand brightness-down () ()
+  "turn down the brightness with brightnessctl"
+  (run-shell-command "brightnessctl set 5%-"))
+
+(defcommand brightness-up () ()
+  "turn up the brightness with brightnessctl"
+  (run-shell-command "brightnessctl set +5%"))
+
+(define-key *top-map* (kbd "XF86MonBrightnessDown") "brightness-down")
+(define-key *top-map* (kbd "XF86MonBrightnessUp") "brightness-up")
+
+;; volume control
+(defcommand volume-down () ()
+  "turn down the volume with amixer"
+  (run-shell-command "amixer set Master unmute; amixer set Master 2%-"))
+
+(defcommand volume-up () ()
+  "turn up the volume with amixer"
+  (run-shell-command "amixer set Master unmute; amixer set Master 2%+"))
+
+(defcommand toggle-mute () ()
+  "toggle mute with amixer"
+  (run-shell-command "amixer set Master toggle"))
+
+(define-key *top-map* (kbd "XF86AudioLowerVolume") "volume-down")
+(define-key *top-map* (kbd "XF86AudioRaiseVolume") "volume-up")
+(define-key *top-map* (kbd "XF86AudioMute") "mute-toggle")
+
+;; cmus control
+(defcommand music-next () ()
+  "next song"
+  (run-shell-command "cmus-remote --next"))
+
+(defcommand music-prev () ()
+  "next song"
+  (run-shell-command "cmus-remote --prev"))
+
+(defcommand music-pause () ()
+  "next song"
+  (run-shell-command "cmus-remote --pause"))
+
+(define-key *top-map* (kbd "XF86AudioNext") "music-next")
+(define-key *top-map* (kbd "XF86AudioPrev") "music-prev")
+(define-key *top-map* (kbd "XF86AudioPlay") "music-pause")
 
 (ql:quickload "clx-truetype")
 (load-module "ttf-fonts")
