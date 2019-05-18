62bbb9efe5cab28fc645a06f9afa871c98de0d33
Christopher Bayliss <cjb@cjb.sh>
2019-05-18 17:58:03 +1000

    better custom shortcut for browse-url

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -74,7 +74,7 @@
 
 (setq browse-url-browser-function 'w3m-browse-url)
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-(global-set-key "\C-xw" 'browse-url-at-point)
+(global-set-key "\C-cb" 'browse-url-at-point)
 
 ;; enable some modes.
 (show-paren-mode +1)
