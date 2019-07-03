1484afc4383badc44b9fb6f6a5ca84f9811fd19e
Christopher Bayliss <cjb@cjb.sh>
2019-07-03 13:35:02 +1000

    EMACS: switch to dark colour scheme, add d-mode, back to erc-hl-nicks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,7 @@
 
 (setq-default fill-column 79
               save-place t
-              frame-background-mode 'light
+              frame-background-mode 'dark
               indent-tabs-mode nil
               show-trailing-whitespace t)
 
@@ -79,11 +79,13 @@
           ("oftc.net" "#debian-devel" "#debian-next" "#debian-mentors"))
         erc-prompt (lambda () (concat "[" (buffer-name) "]")))
 
+  (autoload 'erc-hl-nicks "erc-hl-nicks")
   (setq-default show-trailing-whitespace nil)
   (show-paren-mode -1)
   (erc-scrolltobottom-enable)
   (erc-notifications-mode +1)
   (erc-spelling-mode +1)
+  (erc-hl-nicks)
 
   ;; make ERC use full buffer width
   (add-to-list 'window-configuration-change-hook
@@ -93,55 +95,8 @@
                (lambda () (set (make-local-variable 'scroll-conservatively) 100)))
 
   ;; fix ERC prompt colors
-  (custom-set-faces '(erc-prompt-face ((t (:foreground "black" :background nil :weight bold)))))
-
-  ;; this highlighting is a slightly modified version of option five from here:
-  ;; https://www.emacswiki.org/emacs/ErcNickColors
-  (defmacro unpack-color (color red green blue &rest body)
-    `(let ((,red   (car ,color))
-           (,green (car (cdr ,color)))
-           (,blue  (car (cdr (cdr ,color)))))
-       ,@body))
-
-  (defun rgb-to-html (color)
-    (unpack-color color red green blue
-                  (concat "#" (format "%02x%02x%02x" red green blue))))
-
-  (defun hexcolor-luminance (color)
-    (unpack-color color red green blue
-                  (floor (+ (* 0.299 red) (* 0.587 green) (* 0.114 blue)))))
-
-  (defun invert-color (color)
-    (unpack-color color red green blue
-                  `(,(- 255 red) ,(- 255 green) ,(- 255 blue))))
-
-  (defun erc-get-color-for-nick (nick dark)
-    (let* ((hash     (md5 (downcase nick)))
-           (red      (mod (string-to-number (substring hash 0 10) 16) 256))
-           (blue     (mod (string-to-number (substring hash 10 20) 16) 256))
-           (green    (mod (string-to-number (substring hash 20 30) 16) 256))
-           (color    `(,red ,green ,blue)))
-      (rgb-to-html (if (if dark (< (hexcolor-luminance color) 85)
-                         (> (hexcolor-luminance color) 170))
-                       (invert-color color)
-                     color))))
-
-  (defun erc-highlight-nicknames ()
-    "highlight erc nicknames with color from my/return-color"
-    (save-excursion
-      (goto-char (point-min))
-      (while (re-search-forward "\\w+" nil t)
-        (let* ((bounds (bounds-of-thing-at-point 'symbol))
-               (nick   (buffer-substring-no-properties (car bounds) (cdr bounds))))
-          (when (erc-get-server-user nick)
-            (put-text-property
-	     (car bounds) (cdr bounds) 'face
-             (cons 'foreground-color (erc-get-color-for-nick nick 'nil)))
-            (add-face-text-property
-	     (car bounds) (cdr bounds) 'bold))))))
-
-  (add-hook 'erc-insert-modify-hook 'erc-highlight-nicknames)
-  (add-hook 'erc-send-modify-hook 'erc-highlight-nicknames)
+  (custom-set-faces '(erc-prompt-face ((t (:foreground "brightwhite" :background nil :weight bold))))
+                    '(erc-input-face ((t (:foreground "white")))))
 
   ;; BEHOLD!! this lone paren, isn't it beautiful? One must wonder what life it
   ;; has lived, but since you know how to use git you'll find out in no time!!
@@ -156,6 +111,17 @@
                                (setq c-basic-offset 4)
                                (php-enable-psr2-coding-style))))
 
+;; d-mode
+(add-to-list 'auto-mode-alist
+             '("\\.d\\'" . (lambda ()
+                             (autoload 'd-mode "d-mode")
+                             (d-mode)
+                             (setq c-basic-offset 4))))
+
+
 ;; beleive it or not, this **doesn't** increase emacs init time
 (custom-set-faces
- '(mode-line-buffer-id ((t (:foreground "VioletRed4" :background nil :weight bold :slant oblique)))))
+ '(line-number-current-line ((t (:background "color-237" :foreground "chocolate1"))))
+ '(mode-line-buffer-id ((t (:foreground "red" :background nil :weight bold :slant oblique))))
+ '(region ((t (:inverse-video t))))
+ '(show-paren-match ((t (:foreground "steelblue1")))))
