df7e65840cc21690b6506fa492c6348aa6ca7ccb
Christopher Bayliss <cjb@cjb.sh>
2021-07-28 18:26:52 +1000

    enable the default hardened profile provided with NixOS

--- etc/nixos/harden.nix
+++ etc/nixos/harden.nix
@@ -1,10 +1,18 @@
 { config, pkgs, lib, ... }:
 
 {
+  # import the activly maintained hardend profile, you can find it here:
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/hardened.nix
+  imports = [
+    <nixpkgs/nixos/modules/profiles/hardened.nix>
+  ];
+
   # TODO: finish the job cjb!
-  # harden systemd services. See: systemd-analyze security
-  # start by disabling any service and deps that we don't need:
+  # harden systemd services. See: systemd-analyze security start by
+  # disabling any service and deps that we don't need:
   system.nssModules = lib.mkForce [];
   services.nscd.enable = false;
   users.ldap.nsswitch = false;
+
+  services.dbus.apparmor = "enabled";
 }
