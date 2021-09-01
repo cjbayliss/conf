9e476b98422f4a1d53dbcca697acb4eed32d7540
Christopher Bayliss <cjb@cjb.sh>
2021-09-01 18:51:28 +1000

    reformat haskell with hindent

--- .config/xmonad/bar.hs
+++ .config/xmonad/bar.hs
@@ -1,24 +1,40 @@
 Config
-    { font = "xft:Iosevka-10.5:semibold"
-    , additionalFonts = ["xft:Iosevka-10.5:bold"]
+  { font = "xft:Iosevka-10.5:semibold"
+  , additionalFonts = ["xft:Iosevka-10.5:bold"]
     -- , borderColor = "#608B4E"
     -- , border = BottomB
-    , bgColor = "#0f0f0f"
-    , fgColor = "#d4d4d4"
-    , position = TopW L 100
-    , commands =
-          [ Run ComX "sh" ["-c", "tail -n1 /proc/net/arp | cut -d\" \" -f1"] "DOWN" "arp" 10
-          , Run Alsa
-                "default"
-                "Master"
-                ["-t", "<status>VOL: <volume>%", "--", "-O", "", "-o", "AUDIO OFF ", "-c", "#F44747"]
-          , Run Memory ["-t", "<used>M/<total>M (<usedratio>%)"] 10
-          , Run Com "head" ["-c4", "/proc/loadavg"] "loadavg" 10
-          , Run Date "%0e %^a %H:%M" "date" 10
-          , Run StdinReader
+  , bgColor = "#0f0f0f"
+  , fgColor = "#d4d4d4"
+  , position = TopW L 100
+  , commands =
+      [ Run
+          ComX
+          "sh"
+          ["-c", "tail -n1 /proc/net/arp | cut -d\" \" -f1"]
+          "DOWN"
+          "arp"
+          10
+      , Run
+          Alsa
+          "default"
+          "Master"
+          [ "-t"
+          , "<status>VOL: <volume>%"
+          , "--"
+          , "-O"
+          , ""
+          , "-o"
+          , "AUDIO OFF "
+          , "-c"
+          , "#F44747"
           ]
-    , sepChar = "%"
-    , alignSep = "}{"
-    , template =
-          " %StdinReader% }{ <fc=#569CD6>%alsa:default:Master%</fc> <fc=#333333>│</fc> <fc=#608B4E>%memory%</fc> <fc=#333333>│</fc> <fc=#C678DD>%arp%</fc> <fc=#333333>│</fc> %loadavg% <fc=#333333>│</fc> <fn=1><fc=#DCDCAA>%date%</fc></fn> "
-    }
+      , Run Memory ["-t", "<used>M/<total>M (<usedratio>%)"] 10
+      , Run Com "head" ["-c4", "/proc/loadavg"] "loadavg" 10
+      , Run Date "%0e %^a %H:%M" "date" 10
+      , Run StdinReader
+      ]
+  , sepChar = "%"
+  , alignSep = "}{"
+  , template =
+      " %StdinReader% }{ <fc=#569CD6>%alsa:default:Master%</fc> <fc=#333333>│</fc> <fc=#608B4E>%memory%</fc> <fc=#333333>│</fc> <fc=#C678DD>%arp%</fc> <fc=#333333>│</fc> %loadavg% <fc=#333333>│</fc> <fn=1><fc=#DCDCAA>%date%</fc></fn> "
+  }
