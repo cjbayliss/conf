b747dc7651ef5e6bbe18ab68e3aab9e482cdf3d5
Christopher Bayliss <cjb@cjb.sh>
2021-10-31 17:19:49 +1100

    remove pointless 'else'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,81 +28,79 @@ if not (status) then
     vim.api.nvim_command('packadd packer.nvim')
     firstRun = true
     goto PackerConfig
-else
-    packer.startup(function()
-        use 'wbthomason/packer.nvim'
-
-        use {
-            'Mofiqul/vscode.nvim',
-            config = function()
-                vim.g.vscode_style = 'dark'
-                vim.cmd('colorscheme vscode')
-                vim.api.nvim_command('highlight CursorLine guibg=#1f1f1f')
-                vim.api.nvim_command(
-                    'highlight CursorLineNr guifg=#ffaf00 guibg=#1f1f1f')
-                vim.api.nvim_command('highlight EndOfBuffer guifg=#D4D4D4')
-                vim.api.nvim_command('highlight LineNr guibg=#151515')
-                vim.api.nvim_command('highlight ModeMsg guibg=none')
-                vim.api.nvim_command('highlight Normal guibg=#0f0f0f')
-                vim.api.nvim_command('highlight NormalFloat guibg=#0f0f0f')
-                vim.api.nvim_command('highlight SignColumn guibg=none')
-                vim.api.nvim_command('highlight StatusLine guibg=#151515')
-                vim.api.nvim_command('highlight StatusLineNC guibg=none')
-                vim.api.nvim_command('highlight VertSplit guibg=none')
-            end
-        }
-
-        use {
-            'ethanholz/nvim-lastplace',
-            config = function() require('nvim-lastplace').setup() end
-        }
-
-        use {
-            'lewis6991/gitsigns.nvim',
-            requires = {'nvim-lua/plenary.nvim'},
-            config = function()
-                require('gitsigns').setup({
-                    current_line_blame = true,
-                    current_line_blame_opts = {delay = 500}
-                })
-            end
-        }
-
-        -- remove trailing whitespace automatically
-        use {
-            'lewis6991/spaceless.nvim',
-            config = function() require('spaceless').setup() end
-        }
-
-        use {
-            'norcalli/nvim-colorizer.lua',
-            config = function() require('colorizer').setup() end
-        }
-
-        -- fennel
-        use {'Olical/conjure', ft = 'fennel'}
-        use {
-            'Olical/aniseed',
-            requires = {'Olical/conjure'},
-            config = function()
-                vim.api.nvim_command('packadd packer.nvim')
-            end
-        }
-
-        -- the whole reason to use neovim
-        use {
-            'nvim-treesitter/nvim-treesitter',
-            run = ':TSUpdate',
-            config = function()
-                require('nvim-treesitter.configs').setup({
-                    ensure_installed = 'all',
-                    highlight = {
-                        enable = true,
-                        additional_vim_regex_highlighting = false
-                    }
-                })
-            end
-        }
-        if firstRun then packer.sync() end
-    end)
 end
+
+packer.startup(function()
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'Mofiqul/vscode.nvim',
+        config = function()
+            vim.g.vscode_style = 'dark'
+            vim.cmd('colorscheme vscode')
+            vim.api.nvim_command('highlight CursorLine guibg=#1f1f1f')
+            vim.api.nvim_command(
+                'highlight CursorLineNr guifg=#ffaf00 guibg=#1f1f1f')
+            vim.api.nvim_command('highlight EndOfBuffer guifg=#D4D4D4')
+            vim.api.nvim_command('highlight LineNr guibg=#151515')
+            vim.api.nvim_command('highlight ModeMsg guibg=none')
+            vim.api.nvim_command('highlight Normal guibg=#0f0f0f')
+            vim.api.nvim_command('highlight NormalFloat guibg=#0f0f0f')
+            vim.api.nvim_command('highlight SignColumn guibg=none')
+            vim.api.nvim_command('highlight StatusLine guibg=#151515')
+            vim.api.nvim_command('highlight StatusLineNC guibg=none')
+            vim.api.nvim_command('highlight VertSplit guibg=none')
+        end
+    }
+
+    use {
+        'ethanholz/nvim-lastplace',
+        config = function() require('nvim-lastplace').setup() end
+    }
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = function()
+            require('gitsigns').setup({
+                current_line_blame = true,
+                current_line_blame_opts = {delay = 500}
+            })
+        end
+    }
+
+    -- remove trailing whitespace automatically
+    use {
+        'lewis6991/spaceless.nvim',
+        config = function() require('spaceless').setup() end
+    }
+
+    use {
+        'norcalli/nvim-colorizer.lua',
+        config = function() require('colorizer').setup() end
+    }
+
+    -- fennel
+    use {'Olical/conjure', ft = 'fennel'}
+    use {
+        'Olical/aniseed',
+        requires = {'Olical/conjure'},
+        config = function() vim.api.nvim_command('packadd packer.nvim') end
+    }
+
+    -- the whole reason to use neovim
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = 'all',
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                }
+            })
+        end
+    }
+    if firstRun then packer.sync() end
+end)
