6674890e22d71d780944f51ec72a6097ba2a3b41
Christopher Bayliss <cjb@cjb.sh>
2021-12-12 08:17:42 +1100

    workaround linux kernel *downgrade* in NixOS unstable
    For some reason this was merged https://github.com/NixOS/nixpkgs/pull/147446
    unceremoniously, thus preventing my laptop from booting because it needs
    linux >= 5.14. Serously there should be some process that prevents
    kernal downgrades...

--- etc/nixos/harden.nix
+++ etc/nixos/harden.nix
@@ -9,8 +9,30 @@
   environment.memoryAllocator.provider = "libc";
   # IMPORTANT: don't block modules from being loaded
   security.lockKernelModules = false;
+
   # don't use the 'hardened' kernel
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelPackages = let
+    linux_5_14_pkg = { fetchurl, buildLinux, callPackage, ... } @ args:
+
+      buildLinux (args // rec {
+        patches = callPackage <nixpkgs/pkgs/os-specific/linux/kernel/patches.nix> { };
+        version = "5.14.21";
+        modDirVersion = version;
+
+        src = fetchurl {
+          url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+          sha256 = "1cr381c179nfdrq95l4j56c4ygw09sxv493553ix4b80naf2a6pl";
+        };
+        kernelPatches = [
+          patches.bridge_stp_helper
+          patches.request_key_helper
+        ];
+
+      } // (args.argsOverride or {}));
+    linux_5_14 = pkgs.callPackage linux_5_14_pkg{};
+  in
+    pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_5_14);
+
   # don't disable multi-threading (yeah, ik multi-threading has risks)
   security.allowSimultaneousMultithreading = true;
 
